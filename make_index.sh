a5ea3ec4702fdcb273a41c935ccffcde4cabdeaa
Žiga Žunec <ziga.zunec@gmail.com>
2024-05-14 13:06:47 +0200

    initial auto-generated site

--- make_index.sh
+++ make_index.sh
@@ -4,17 +4,32 @@ ROOT=./slides
 HTTP="/"
 OUTPUT=index.html 
 
-i=0
-echo "<UL>" > $OUTPUT
-for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
-  path=`basename "$filepath"`
-  echo "  <LI>/$ROOT/$path</LI>" >> $OUTPUT
-  echo "  <UL>" >> $OUTPUT
-  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
-    file=`basename "$i"`
-    echo "    <LI><a href=\"/$ROOT/$path/$file\">$file</a></LI>" >> $OUTPUT
-  done
-  echo "  </UL>" >> $OUTPUT
-done
-echo "</UL>" >> $OUTPUT
+echo "<head>" > $OUTPUT
+echo "<style>" >> $OUTPUT
+echo "body{background:#fdf6e3;font-family: Lato;font-size: 20px;font-weight: bold;  line-height: 1.8;color: #586e75;padding: 40px;}" >> $OUTPUT
+echo "a:link { color:#586e75; text-decoration: none; }" >> $OUTPUT
+echo "a:visited { color:#586e75; text-decoration: none; }" >> $OUTPUT
+echo "</style>" >> $OUTPUT
 
+echo "<title>Prezentacije</title>" >> $OUTPUT
+echo "</head>" >> $OUTPUT
+
+
+echo "<ul>" >> $OUTPUT
+for file in "$ROOT"/*/*; do
+  parentpath="${file#*/}"
+  parent="${parentpath%/*}"
+  parent=${parent##*/}
+  filename="${file##*/}"
+  if [[ -z $oldparent ]]; then
+    echo "  <li> $parent </li>" >> $OUTPUT && oldparent="$parent" 
+    echo "  <ul>" >> $OUTPUT
+  elif [[ $oldparent != $parent ]]; then
+    echo "  </ul>" >> $OUTPUT
+    echo "  <li> $parent </li>" >> $OUTPUT && oldparent="$parent"
+    echo "  <ul>" >> $OUTPUT
+  fi
+  echo "    <li><a href=\"$parentpath\">${filename%%.*}</a></li>" >> $OUTPUT
+done
+echo "  </ul>" >> $OUTPUT
+echo "</ul>" >> $OUTPUT
