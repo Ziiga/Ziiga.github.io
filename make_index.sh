66a80126bab14241ad42473237782bfb2e36c4cc
Žiga Žunec <ziga.zunec@gmail.com>
2024-05-14 10:29:35 +0200

    autogen test 2

--- make_index.sh
+++ make_index.sh
@@ -8,11 +8,11 @@ i=0
 echo "<UL>" > $OUTPUT
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   path=`basename "$filepath"`
-  echo "  <LI>$path</LI>" >> $OUTPUT
+  echo "  <LI>/$ROOT/$path</LI>" >> $OUTPUT
   echo "  <UL>" >> $OUTPUT
   for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
     file=`basename "$i"`
-    echo "    <LI><a href=\"/$path/$file\">$file</a></LI>" >> $OUTPUT
+    echo "    <LI><a href=\"/$ROOT/$path/$file\">$file</a></LI>" >> $OUTPUT
   done
   echo "  </UL>" >> $OUTPUT
 done
